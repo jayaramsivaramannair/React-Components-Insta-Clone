128e4652149d13a4978f16d65a3b9aa1f477190e
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-12-19 04:52:31 -0500

    Added Posts and Comments to the Page by using map function

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  const [posts] = useState(dummyData)
+  const [posts, setPosts] = useState(dummyData)
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -30,13 +30,24 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+     setPosts(() => {
+       posts.map((post) => {
+         if(post.id === postId) {
+            let newLikes = post.likes + 1;
+            return {...post, likes: newLikes}
+         } else {
+           return post
+         }
+       })
+     })
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts />
+      <Posts posts={posts} likePost = {likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
